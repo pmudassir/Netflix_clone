bc16b526f0785445867ddcd0fe983a708fd7efeb
pmudassir <pmuddasir@gmail.com>
2023-02-11 23:50:36 +0530

    Slider animation given

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,31 @@
 import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@mui/icons-material"
+import { useRef, useState } from "react"
 import ListItem from "../listItem/ListItem"
 import "./list.scss"
 
 const List = () => {
+
+    const [slideNumber, setSlideNumber] = useState()
+
+    const listRef = useRef()    //ref hook is used in react instead of the plain js function like get element by id
+
+    const handleClick = (direction) => {
+        let distance = listRef.current.getBoundingClientRect().x - 50  //between the listItem and the x axis
+        if (direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1)
+            listRef.current.style.transform = `translate(${230 + distance}px)`
+        }
+        if (direction === "right" && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1)
+            listRef.current.style.transform = `translate(${-230 + distance}px)`
+        }
+    }
     return (
         <div className="list">
             <span className="listTitle">Continue Watching</span>
             <div className="wrapper">
-                <ArrowBackIosOutlined className="sliderArrow left" />
-                <div className="container">
+                <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick("left")} />
+                <div className="container" ref={listRef}>
                     <ListItem />
                     <ListItem />
                     <ListItem />
@@ -20,7 +37,7 @@ const List = () => {
                     <ListItem />
                     <ListItem />
                 </div>
-                <ArrowForwardIosOutlined className="sliderArrow right" />
+                <ArrowForwardIosOutlined className="sliderArrow right" onClick={() => handleClick("right")} />
             </div>
         </div>
     )
