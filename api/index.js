ed07d81245b497ebd9c9f4ffdfe06a6eaa6a3c82
pmudassir <pmuddasir@gmail.com>
2023-02-14 22:12:21 +0530

    First commit

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+const app = express()
+dotenv.config()
+
+mongoose.set('strictQuery', true);
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connection Successful")
+    }).catch((err) => {
+        console.log(err);
+    });
+
+app.listen(8800, () => {
+    console.log("Server running on port 8800");
+})
---
+++ api/routes/auth.js
@@ -0,0 +1,6 @@
+const router = require("express").Router()
+const User = require("../models/User")
+
+router.post("/register", (req,res) => {
+    res.send("register")
+})
