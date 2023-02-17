afbec9a929ae8bf4f2d329ee9148938523ea8b83
pmudassir <pmuddasir@gmail.com>
2023-02-17 20:35:30 +0530

    movie crud finished

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
 
 const app = express()
 dotenv.config()
@@ -23,6 +24,7 @@ app.use(express.json())
 
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/movies", movieRoute)
 
 app.listen(8800, () => {
     console.log("Server running on port 8800");
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -51,12 +51,49 @@ router.delete("/:id", verify, async (req, res) => {
 })
 
 //GET
-router.get("/:id", verify, async (req, res) => {
+router.get("/find /:id", verify, async (req, res) => {
     try {
-        await Movie.findByIdAndDelete(req.params.id)
-        res.status(200).json("Movie has been deleted...")
+        const movie = await Movie.findById(req.params.id)
+        res.status(200).json(movie)
     } catch (error) {
         res.status(500).json(error)
     }
 })
+
+//RANDOM
+router.get("/random", verify, async (req, res) => {
+    const type = req.query.type;
+    let movie;
+    try {
+        if (type === "series") {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: true } },
+                { $sample: { size: 1 } }    //gets all series and returns a random one
+            ])
+        } else {
+            movie = await Movie.aggregate([
+                { $match: { isSeries: false } },
+                { $sample: { size: 1 } }
+            ])
+        }
+        res.status(200).json(movie)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//GET ALL
+router.get("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const movies = await Movie.find()
+            res.status(200).json(movies.reverse())
+        } catch (error) {
+            res.status(500).json(error)
+        }
+    } else {
+        res.status(403).json("You are not an admin!")
+    }
+})
+
 module.exports = router;
