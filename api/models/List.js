55a948bdcdb3b67f71897a33d07562836163fe94
pmudassir <pmuddasir@gmail.com>
2023-02-18 17:15:32 +0530

    list crud completed

--- api/models/List.js
+++ api/models/List.js
@@ -8,4 +8,4 @@ const ListSchema = new mongoose.Schema(
         content: { type: Array, },
     }, { timestamps: true })
 
-module.exporst = mongoose.model("List", ListSchema)
+module.exports = mongoose.model("List", ListSchema)
