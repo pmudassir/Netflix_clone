bf9c4fdd41f04e97c14ed87af6726464a91680f5
pmudassir <pmuddasir@gmail.com>
2023-02-19 13:18:57 +0530

    routing completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import "./app.scss"
-import Login from "./pages/login/Login";
+import Login from "./pages/login/Login"
+import Home from "./pages/home/Home"
+import Register from "./pages/register/Register"
+import Watch from "./pages/watch/Watch"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
+  const user = true
   return (
-    <div className="App"><Login /></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? (<Home />) : <Navigate to="/register" />} />
+        <Route path="/login" element={!user ? (<Login />) : <Navigate to="/" />} />
+        <Route path="/register" element={!user ? (<Register />) : <Navigate to="/" />} />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { ArrowDropDown, Notifications, Search } from "@mui/icons-material"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 import "./navbar.scss"
 
 const Navbar = () => {
@@ -11,13 +12,19 @@ const Navbar = () => {
     }
 
     return (
-        <div className= {isScrolled ? "navbar scrolled" : "navbar"}>
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
             <div className="container">
                 <div className="left">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
-                    <span>Home</span>
-                    <span>Series</span>
-                    <span>Movies</span>
+                    <Link to="/" className="link">
+                        <span>Home</span>
+                    </Link>
+                    <Link to="/series" className="link">
+                        <span>Series</span>
+                    </Link>
+                    <Link to="/movies" className="link">
+                        <span>Movies</span>
+                    </Link>
                     <span>New & Popular</span>
                     <span>My List</span>
                 </div>
