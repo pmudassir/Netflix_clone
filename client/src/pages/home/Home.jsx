ea6220920cc51f40ad0b9b3a59ffd50b61b983cf
pmudassir <pmuddasir@gmail.com>
2023-02-19 20:27:24 +0530

    list api completed

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react"
 import Featured from "../../components/featured/Featured"
 import List from "../../components/list/List"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.scss"
+import axios from "axios"
 
 const Home = ({ type }) => {
+  const [lists, setLists] = useState([])
+  const [genre, setGenre] = useState(null)
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(`lists${type ? "?type" + type : ""}${genre ? "&genre" + genre : ""}`)
+        console.log(res);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getRandomLists()
+  }, [type, genre])
   return (
     <div className="home">
       <Navbar />
